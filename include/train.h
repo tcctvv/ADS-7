9e6fed677543c7893071f74903daeba2092b795f
tcctvv <anastasiazhiharewa@yandex.ru>
2025-05-18 19:26:00 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -2,25 +2,27 @@
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
+#include <cstdint>
+
 class Train {
 private:
     struct Car {
-        bool light;
-        Car* next;
-        Car* prev;
+        bool light{};
+        Car* next{};
+        Car* prev{};
     };
 
-    Car* first;
-    int   size;
-    long  countOp;
+    Car*      first{nullptr};
+    int       size{0};
+    int64_t   opCount{0};
 
 public:
-    Train();
+    Train()  = default;
     ~Train();
 
-    void addCar(bool light);
-    int  getLength();
-    long getOpCount() const;
+    void   addCar(bool light);
+    int    getLength();
+    int64_t getOpCount() const { return opCount; }
 };
 
 #endif  // INCLUDE_TRAIN_H_
